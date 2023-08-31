2defcccf1c8e04a22ff72445085d0fad00cae26d
cyy280113999 <cyy280113999@gmail.com>
2023-08-31 14:26:00 +0800

    config

--- global_config.py
+++ global_config.py
@@ -7,6 +7,7 @@ AtRemote=True # is server at remote ?
 # usr_name = 'cyy'
 usr_name = 'cyy' if not AtRemote else 'lhk'
 # ==main
+
 ON_LINE=True  # where is pc?
 Start_MServer=False
 Start_MClient=True
