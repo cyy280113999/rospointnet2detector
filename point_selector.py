1badcb8103f0079975700616b04662dcb391cbe7
cyy280113999 <cyy280113999@gmail.com>
2023-09-14 15:08:21 +0800

    new bound

--- point_selector.py
+++ point_selector.py
@@ -6,7 +6,7 @@ from utilities import *
 import open3d as o3d
 import global_config as CONF
 
-BoundShrink=0.20
+BoundShrink=0.30
 DIST_ROD=0.20
 detect_box=np.array([[-1000, 1492,-1300],
                      [-6900, 2531,  465]],dtype=np.float32)
@@ -38,85 +38,84 @@ class PointSelector:
         # return: list->[points]. None->error 
         if CONF.DEBUG:
             pc_log=x
-        avoid_y = x[(x[:,3]==4).__or__(x[:,3]==5),1] # exclude rod and rain
+        # select material(3) and target(6)
+        material = x[(x[:,3]==3).__or__(x[:,3]==6)] 
+        avoid_y = x[(x[:,3]==4).__or__(x[:,3]==5),1]
+        # remove outliner/noise
+        pc = o3d.geometry.PointCloud() 
+        pc.points = o3d.utility.Vector3dVector(material[:,:3])
+        pc,ind = pc.remove_radius_outlier(nb_points=3,radius=0.20) 
+        material = np.concatenate([np.array(pc.points,dtype=np.float32),material[ind,3,None].astype(np.float32)],axis=1)
+        # escape from rod and rain
         if len(avoid_y)>0:
-            y_intervals=find_clusters(avoid_y)
+            y_intervals = find_clusters(avoid_y)
             y_intervals = interval_expand(y_intervals, radius=DIST_ROD)
-            x = inverse_select(x,y_intervals)
-        x = pc_bound(x,xb=detect_box[0],yb=detect_box[1],zb=detect_box[2])
-        # x = pc_bound(x,xb=bbox2[0],yb=bbox2[1],zb=bbox2[2])
-        material = x[(x[:,3]==3).__or__(x[:,3]==6)] # material and target
+            material = inverse_select(material,y_intervals)
+        # bound shrink
+        UB=np.max(material,axis=0)# up bound
+        DB=np.min(material,axis=0)# down bound
+        material = pc_bound(material,yb=(DB[1]+BoundShrink,UB[1]-BoundShrink),zb=(DB[2]+BoundShrink,UB[2]-BoundShrink)) 
+        # bounding as arm avaiable region
+        material = pc_bound(material,xb=detect_box[0],yb=detect_box[1],zb=detect_box[2])  
         if len(material)==0:
             points=None
         else:
-            pc = o3d.geometry.PointCloud()
-            pc.points = o3d.utility.Vector3dVector(material[:,:3])
-            pc,ind = pc.remove_radius_outlier(nb_points=3,radius=0.20)
-            material = np.concatenate([np.array(pc.points,dtype=np.float32),material[ind,3,None].astype(np.float32)],axis=1)
-            # o3d.visualization.draw_geometries([pc,],window_name="pc down",width=800,height=600)
-            if len(material)==0:
+            bound = np.max(material,axis=0)
+            yM=bound[1]
+            zM=bound[2]
+            bound = material.min(axis=0)
+            ym=bound[1]
+            zm=bound[2]
+            ysteps=5
+            zsteps=3
+            patches=[]
+            ystep=(yM-ym)/ysteps
+            zstep=(zM-zm)/zsteps
+            if CONF.DEBUG:
+                pc_plot=[]# plot patches
+            # split area into 15 patches
+            for i in range(ysteps):
+                indices=(material[:,1]>=(ym+i*ystep)).__and__(material[:,1]<=(ym+(i+1)*ystep))
+                row=material[indices]
+                material=material[~indices]
+                for j in range(zsteps):
+                    indices=(row[:,2]>=(zm+j*zstep)).__and__(row[:,2]<=(zm+(j+1)*zstep))
+                    col=row[indices]
+                    row=row[~indices]
+                    if len(col)>0:
+                        col[:,3]=class_num+i*zsteps+j # over write intensity
+                        patches.append(col)
+            if len(patches)<n//2: # less than half
                 points=None
             else:
-                bound = np.max(material,axis=0)
-                yM=bound[1]-BoundShrink
-                zM=bound[2]#-BoundShrink
-                bound = material.min(axis=0)
-                ym=bound[1]+BoundShrink
-                zm=bound[2]+BoundShrink
-                # if CONF.DEBUG: # not record material. 
-                #     pc_plot=material+np.array([[-0.2,0,0,0]])
-                #     pc_log=np.vstack([pc_log,pc_plot])
-                #     self.pub_mater.publish(rosnp.point_cloud2.array_to_pointcloud2(np_xyzi2pc(pc_plot),frame_id=CONF.lidar_frame))
-                ysteps=5
-                zsteps=3
-                patches=[]
-                ystep=(yM-ym)/ysteps
-                zstep=(zM-zm)/zsteps
                 if CONF.DEBUG:
-                    pc_plot=[]# plot patches
-                # split area into 15 patches
-                for i in range(ysteps):
-                    indices=(material[:,1]>=(ym+i*ystep)).__and__(material[:,1]<=(ym+(i+1)*ystep))
-                    row=material[indices]
-                    material=material[~indices]
-                    for j in range(zsteps):
-                        indices=(row[:,2]>=(zm+j*zstep)).__and__(row[:,2]<=(zm+(j+1)*zstep))
-                        col=row[indices]
-                        row=row[~indices]
-                        if len(col)>0:
-                            col[:,3]=class_num+i*zsteps+j # over write intensity
-                            patches.append(col)
-                if len(patches)<n//2: # less than half
-                    points=None
+                    pc_plot=np.vstack(patches)+np.array([[-0.4,0,0,0]])
+                    pc_log=np.vstack([pc_log,pc_plot])
+                    self.pub_patch.publish(rosnp.point_cloud2.array_to_pointcloud2(np_xyzi2pc(pc_plot),frame_id=CONF.FRAME_LIDAR))
+                # get max point of each patch
+                points=[]
+                for i in range(len(patches)):
+                    patch=patches[i]
+                    # ind=patch[:,0].argmin(axis=0) # hightest. min in x axis 
+                    h=patch[:,0]
+                    ind=find_nearst(h,h.mean())
+                    patch[ind,3]=0 # point with zero intensity
+                    points.append(patch[ind])
+                points=np.vstack(points)
+                if len(points)<n//2: # fill the list, ensure enough to 15
+                    count_lack=n//2-len(points)
+                    ind_add=np.random.choice(len(points),count_lack)
+                    points=np.vstack([points,points[ind_add]])
+                points=points[np.argsort(points[:,1])[::-1]] # sort by y
+                if n==2:
+                    points = points[[0,-1]]
                 else:
-                    if CONF.DEBUG:
-                        pc_plot=np.vstack(patches)+np.array([[-0.4,0,0,0]])
-                        pc_log=np.vstack([pc_log,pc_plot])
-                        self.pub_patch.publish(rosnp.point_cloud2.array_to_pointcloud2(np_xyzi2pc(pc_plot),frame_id=CONF.FRAME_LIDAR))
-                    # get max point of each patch
-                    points=[]
-                    for i in range(len(patches)):
-                        patch=patches[i]
-                        # ind=patch[:,0].argmin(axis=0) # hightest. min in x axis 
-                        h=patch[:,0]
-                        ind=find_nearst(h,h.mean())
-                        patch[ind,3]=0 # point with zero intensity
-                        points.append(patch[ind])
-                    points=np.vstack(points)
-                    if len(points)<n//2: # fill the list, ensure enough to 15
-                        count_lack=n//2-len(points)
-                        ind_add=np.random.choice(len(points),count_lack)
-                        points=np.vstack([points,points[ind_add]])
-                    points=points[np.argsort(points[:,1])[::-1]] # sort by y
-                    if n==2:
-                        points = points[[0,-1]]
-                    else:
-                        points = points[:13]
-                    if CONF.DEBUG:
-                        pc_plot=points+np.array([[-0.8,0,0,0]])
-                        pc_log=np.vstack([pc_log,pc_plot])
-                        self.pub_max.publish(rosnp.point_cloud2.array_to_pointcloud2(np_xyzi2pc(pc_plot),frame_id=CONF.FRAME_LIDAR))
-                        self.pub_log.publish(rosnp.point_cloud2.array_to_pointcloud2(np_xyzi2pc(pc_log),frame_id=CONF.FRAME_LIDAR))
+                    points = points[:13]
+                if CONF.DEBUG:
+                    pc_plot=points+np.array([[-0.8,0,0,0]])
+                    pc_log=np.vstack([pc_log,pc_plot])
+                    self.pub_max.publish(rosnp.point_cloud2.array_to_pointcloud2(np_xyzi2pc(pc_plot),frame_id=CONF.FRAME_LIDAR))
+                    self.pub_log.publish(rosnp.point_cloud2.array_to_pointcloud2(np_xyzi2pc(pc_log),frame_id=CONF.FRAME_LIDAR))
         if CONF.DEBUG:
             pcd = o3d.t.geometry.PointCloud(o3d.core.Tensor(pc_log[:,:3].astype(np.float32))) # set position
             pcd.point.intensity=o3d.core.Tensor(pc_log[:,3:4].astype(np.float32)) # set intensity 
